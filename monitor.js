1a1a1d1991ca29ace24ed4c34af4dc62cb76b7d1
Timur Kiyivinski <deathseclipse@gmail.com>
2016-11-01 20:36:18 +0800

    [ADD] Suprnova workers [REFACTOR] Positioning

--- monitor.js
+++ monitor.js
@@ -6,6 +6,7 @@ const request = require('request')
 
 const seconds = val => val * 1000
 const pointArr = (size, filler) => Array(size).fill(filler)
+const flatten = arr => [].concat(...arr)
 
 ;(function () {
   // Environment variables
@@ -65,6 +66,35 @@ const pointArr = (size, filler) => Array(size).fill(filler)
     return market
   }
 
+  const createSuprnovaSeries = pool => ({
+    name: pool.name,
+    title: `${pool.name}`,
+    style: pool.style,
+    interval: pool.interval,
+    api_key: pool.api_key,
+    x: pointArr(env.novaexchange.points, '*'),
+    y: pointArr(env.novaexchange.points, 0)
+  })
+
+  const createSuprnovaFunction = pool => {
+    const createSuprnovaFunctionCall = () => {
+      request(`https://${pool.name}.suprnova.cc/index.php?page=api&action=getuserworkers&api_key=${pool.api_key}`, (err, response, body) => {
+        if (!err && response.statusCode === 200) {
+          const data = JSON.parse(body)
+          const hashrate = data.getuserworkers.data
+            .map(worker => worker.hashrate)
+            .reduce((a, b) => a + b)
+          pool.title = `${pool.name} ${hashrate}`
+          pool.y.shift()
+          pool.y.push(hashrate)
+        }
+      })
+    }
+    createSuprnovaFunctionCall()
+    setInterval(createSuprnovaFunctionCall, seconds(pool.interval))
+    return pool
+  }
+
   // Create screen
   const screen = blessed.screen({
     smartSCR: true
@@ -103,9 +133,9 @@ const pointArr = (size, filler) => Array(size).fill(filler)
 
   const novaExchangeBox = blessed.box({
     top: '30%',
-    left: 'center',
-    width: '100%',
-    height: '70%',
+    left: 'left',
+    width: '40%',
+    height: '30%',
     border: {
       type: 'line'
     }
@@ -128,10 +158,38 @@ const pointArr = (size, filler) => Array(size).fill(filler)
   screen.append(novaExchangeBox)
   novaExchangeBox.append(novaExchangeChart)
 
+  const suprnovaBox = blessed.box({
+    top: '30%',
+    left: '40%+1',
+    width: '60%',
+    height: '30%',
+    border: {
+      type: 'line'
+    }
+  })
+
+  const suprnovaChart = contrib.line({
+    style: env.system.style,
+    xLabelPadding: 3,
+    xPadding: 5,
+    showLegend: true,
+    wholeNumbersOnly: false,
+    label: 'Suprnova Hashrate'
+  })
+
+  const suprnovaSeries = env.suprnova.pools
+    .map(createSuprnovaSeries)
+    .map(createSuprnovaFunction)
+
+  // Add NovaExchange charts
+  screen.append(suprnovaBox)
+  suprnovaBox.append(suprnovaChart)
+
   // Render screen
   setInterval(function () {
     sysChart.setData(sysSeries)
     novaExchangeChart.setData(novaExchangeSeries)
+    suprnovaChart.setData(suprnovaSeries)
     screen.render()
   }, seconds(1))
 })()
